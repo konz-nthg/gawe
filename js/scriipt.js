9b76c0395c9a2d80641c18195aa721e31a70b173
konz-nthg <rhmtmaulana31@gmail.com>
2026-04-09 14:04:34 +0700

    .

--- js/scriipt.js
+++ js/scriipt.js
@@ -4,12 +4,32 @@ const stopMode = document.getElementById('stopMode');
 const btnDraw = document.getElementById('btnDraw');
 const btnStop = document.getElementById('btnStop');
 
+// klik draw mode
 btnDraw.addEventListener('click', () => {
-  drawMode.classList.add('active');
-  stopMode.classList.remove('active');
+  drawMode.style.zIndex = 2;
+  stopMode.style.zIndex = 1;
 });
 
+// klik stop motion
 btnStop.addEventListener('click', () => {
-  stopMode.classList.add('active');
-  drawMode.classList.remove('active');
+  stopMode.style.zIndex = 2;
+  drawMode.style.zIndex = 1;
 });
+
+const makeLobby = document.getElementById('makeLobby');
+const joinLobby = document.getElementById('joinLobby');
+
+const btnMake = document.getElementById('btnMake');
+const btnJoin = document.getElementById('btnJoin');
+
+document.getElementById('btnMake').addEventListener('click', () => {
+  makeLobby.style.zIndex = 20;
+  joinLobby.style.zIndex = 10;
+});
+
+// klik stop motion
+document.getElementById('btnJoin').addEventListener('click', () => {
+  makeLobby.style.zIndex = 10;
+  joinLobby.style.zIndex = 20;
+});
+
